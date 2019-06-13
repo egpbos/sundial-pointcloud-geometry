47c3e4651f72de961bb700d7c4a64655fa3f30ac
E. G. Patrick Bos <egpbos@gmail.com>
2019-06-13 16:07:23 +0200

    split into plydata and df handling functions

--- explore.py
+++ explore.py
@@ -1,5 +1,15 @@
-import plyfile
 import numpy as np
+import logging
+# data formats
+import plyfile
+import pandas as pd
+import vaex as vx
+# visualization
+import ipyvolume as ipv
+import pptk
+
+logger = logging.getLogger('sundial-pointcloud-geometry.explore')
+logger.setLevel(logging.DEBUG)
 
 
 # BEGIN generic things
@@ -7,41 +17,73 @@ def open_plyfile(filename):
     plydata = plyfile.PlyData.read(filename)
     return plydata
 
-def ipv_plot(plydata, sample_frac=1, **kwargs):
-    import ipyvolume
-    
+
+def random_sample(xyz, total, sample_frac):
+    sample = np.random.choice(total, int(sample_frac * total), replace=False)
+    logger.debug("sample size:", int(sample_frac * total), "out of total", total)
+    return dict(x=xyz['x'][sample], y=xyz['y'][sample], z=xyz['z'][sample])
+
+
+def ipv_plot_plydata(plydata, sample_frac=1, **kwargs):
     if sample_frac < 1:
-        sample = np.random.choice(plydata['vertex'].count, int(sample_frac * plydata['vertex'].count), replace=False)
-        print("sample size:", int(sample_frac * plydata['vertex'].count), "out of total", plydata['vertex'].count)
-        xyz = dict(x=plydata['vertex']['x'][sample], y=plydata['vertex']['y'][sample], z=plydata['vertex']['z'][sample])
+        xyz = random_sample(plydata['vertex'], plydata['vertex'].count, sample_frac)
     else:
         xyz = dict(x=plydata['vertex']['x'], y=plydata['vertex']['y'], z=plydata['vertex']['z'])
-    ipyvolume.scatter(**xyz, **kwargs)
-    ipyvolume.show()
+    ipv.scatter(**xyz, **kwargs)
+    ipv.show()
 
 
-def pptk_plot(plydata, **kwargs):
-    import pptk
-    
+def pptk_plot_plydata(plydata, **kwargs):
     pptk.viewer(np.array([plydata['vertex']['x'], plydata['vertex']['y'], plydata['vertex']['z']]).T)
+
+
+def ipv_plot_df(df, sample_frac=1, **kwargs):
+    if sample_frac < 1:
+        xyz = random_sample(df, len(df), sample_frac)
+    else:
+        xyz = dict(x=df['x'], y=df['y'], z=df['z'])
+    ipv.scatter(**xyz, **kwargs)
+    ipv.show()
+
+
+def pptk_plot_df(df, **kwargs):
+    pptk.viewer(np.array([df['x'], df['y'], df['z']]).T)
+
+
+def vertex_dict_from_plyfile(filename):
+    plydata = open_plyfile(filename)
+    xyz = dict(x=plydata['vertex']['x'], y=plydata['vertex']['y'], z=plydata['vertex']['z'])
+    return xyz
+
+
+def pandas_vertices_from_plyfile(filename):
+    xyz = vertex_dict_from_plyfile(filename)
+    return pd.DataFrame(xyz)
+
+
+def vaex_vertices_from_plyfile(filename):
+    xyz = vertex_dict_from_plyfile(filename)
+    return vx.from_dict(xyz)
+
 
 # END generic things
 
 
-
 # BEGIN really specific to example dataset used for exploration
 def load_example_cube():
-    plydata = open_plyfile('cube.ply')
+    plydata = open_plyfile('data/cube.ply')
 
     return plydata
+
 
 def load_example():
-    plydata = open_plyfile('ObjID126.ply')
+    plydata = open_plyfile('data/ObjID126.ply')
 
     return plydata
 
+
 def load_example_full():
-    plydata = open_plyfile('ObjID126.full.ply')
+    plydata = open_plyfile('data/ObjID126.full.ply')
 
     return plydata
 
