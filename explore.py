017d71e3a6b15d8ec31ae1141e313e06bfb26e43
E. G. Patrick Bos <egpbos@gmail.com>
2019-06-17 08:42:31 +0200

    move visualization to ectopylasm

--- explore.py
+++ explore.py
@@ -4,9 +4,6 @@ import logging
 import plyfile
 import pandas as pd
 import vaex as vx
-# visualization
-import ipyvolume as ipv
-import pptk
 
 logger = logging.getLogger('sundial-pointcloud-geometry.explore')
 logger.setLevel(logging.DEBUG)
@@ -16,38 +13,6 @@ logger.setLevel(logging.DEBUG)
 def open_plyfile(filename):
     plydata = plyfile.PlyData.read(filename)
     return plydata
-
-
-def random_sample(xyz, total, sample_frac):
-    sample = np.random.choice(total, int(sample_frac * total), replace=False)
-    logger.debug("sample size:", int(sample_frac * total), "out of total", total)
-    return dict(x=xyz['x'][sample], y=xyz['y'][sample], z=xyz['z'][sample])
-
-
-def ipv_plot_plydata(plydata, sample_frac=1, **kwargs):
-    if sample_frac < 1:
-        xyz = random_sample(plydata['vertex'], plydata['vertex'].count, sample_frac)
-    else:
-        xyz = dict(x=plydata['vertex']['x'], y=plydata['vertex']['y'], z=plydata['vertex']['z'])
-    ipv.scatter(**xyz, **kwargs)
-    ipv.show()
-
-
-def pptk_plot_plydata(plydata, **kwargs):
-    pptk.viewer(np.array([plydata['vertex']['x'], plydata['vertex']['y'], plydata['vertex']['z']]).T)
-
-
-def ipv_plot_df(df, sample_frac=1, **kwargs):
-    if sample_frac < 1:
-        xyz = random_sample(df, len(df), sample_frac)
-    else:
-        xyz = dict(x=df['x'], y=df['y'], z=df['z'])
-    ipv.scatter(**xyz, **kwargs)
-    ipv.show()
-
-
-def pptk_plot_df(df, **kwargs):
-    pptk.viewer(np.array([df['x'], df['y'], df['z']]).T)
 
 
 def vertex_dict_from_plyfile(filename):
