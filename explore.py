aff6ff85e41cd4d1d270610989e52d3b05af2303
E. G. Patrick Bos <egpbos@gmail.com>
2019-06-17 08:51:38 +0200

    move io functions to ectopylasm

--- explore.py
+++ explore.py
@@ -1,34 +1,11 @@
-import numpy as np
 import logging
-# data formats
-import plyfile
-import pandas as pd
-import vaex as vx
+from ectopylasm import load_plyfile
 
 logger = logging.getLogger('sundial-pointcloud-geometry.explore')
 logger.setLevel(logging.DEBUG)
 
 
 # BEGIN generic things
-def open_plyfile(filename):
-    plydata = plyfile.PlyData.read(filename)
-    return plydata
-
-
-def vertex_dict_from_plyfile(filename):
-    plydata = open_plyfile(filename)
-    xyz = dict(x=plydata['vertex']['x'], y=plydata['vertex']['y'], z=plydata['vertex']['z'])
-    return xyz
-
-
-def pandas_vertices_from_plyfile(filename):
-    xyz = vertex_dict_from_plyfile(filename)
-    return pd.DataFrame(xyz)
-
-
-def vaex_vertices_from_plyfile(filename):
-    xyz = vertex_dict_from_plyfile(filename)
-    return vx.from_dict(xyz)
 
 
 # END generic things
@@ -36,19 +13,19 @@ def vaex_vertices_from_plyfile(filename):
 
 # BEGIN really specific to example dataset used for exploration
 def load_example_cube():
-    plydata = open_plyfile('data/cube.ply')
+    plydata = load_plyfile('data/cube.ply')
 
     return plydata
 
 
 def load_example():
-    plydata = open_plyfile('data/ObjID126.ply')
+    plydata = load_plyfile('data/ObjID126.ply')
 
     return plydata
 
 
 def load_example_full():
-    plydata = open_plyfile('data/ObjID126.full.ply')
+    plydata = load_plyfile('data/ObjID126.full.ply')
 
     return plydata
 
