34be48dd8b32a077fc57154ed26e2c925c74242a
E. G. Patrick Bos <egpbos@gmail.com>
2019-06-18 15:22:29 +0200

    Replace data files with download script

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -fLo ObjID126.ply http://repository.edition-topoi.org/BSDP/ReposBSDP/BSDP0030/ObjID126.ply
+curl -fLo ObjID126.full.ply http://repository.edition-topoi.org/BSDP/ReposBSDP/BSDP0030/ObjID126.full.ply
